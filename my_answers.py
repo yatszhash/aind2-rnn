16bd9dc03ebbb4a9a35e5634217958b6cf98cf06
Sushi Yatsuhashi <sushi.yatsuhashi@gamil.com>
2017-08-11 11:50:27 +0900

    add window_transform_series, build_part1_RNN and window_transform_text

--- my_answers.py
+++ my_answers.py
@@ -4,14 +4,16 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
 import keras
+import re
 
 
 # TODO: fill out the function below that transforms the input series 
 # and window-size into a set of input/output pairs for use with our RNN model
 def window_transform_series(series, window_size):
     # containers for input/output pairs
-    X = []
-    y = []
+    series_size = series.shape[0]
+    X = [series[i:i + window_size] for i in range(series_size - window_size)]
+    y = [series[i] for i in range(window_size, series_size)]
 
     # reshape each 
     X = np.asarray(X)
@@ -23,20 +25,29 @@ def window_transform_series(series, window_size):
 
 # TODO: build an RNN to perform regression on our time series input/output data
 def build_part1_RNN(window_size):
-    pass
+    model = Sequential()
 
+    model.add(LSTM(5, input_shape=(window_size, 1)))
+    model.add(Dense(1, activation="sigmoid"))
+    return model
 
 ### TODO: return the text input with only ascii lowercase and the punctuation given below included.
 def cleaned_text(text):
     punctuation = ['!', ',', '.', ':', ';', '?']
 
-    return text
+    replace_pattern = r"[^a-z\d\!,\.:;\? ]"
+    # compiled_pattern = re.compile(replace_pattern)
+    # text.lower()
+    replaced_text = re.sub(replace_pattern, ' ', text)
+
+    return replaced_text
 
 ### TODO: fill out the function below that transforms the input text and window-size into a set of input/output pairs for use with our RNN model
 def window_transform_text(text, window_size, step_size):
     # containers for input/output pairs
-    inputs = []
-    outputs = []
+    text_size = len(text)
+    inputs = [list(text[i:i + window_size]) for i in range(text_size - window_size - step_size + 1)]
+    outputs = [list(text[i:i + step_size]) for i in range(window_size, text_size - step_size + 1)]
 
     return inputs,outputs
 
---
+++ test_my_answer.py
@@ -0,0 +1,60 @@
+from unittest import TestCase
+
+import numpy as np
+from my_answers import *
+
+class TestMyAnswer(TestCase):
+    def test_window_transform_series(self):
+        odd_nums = np.array([1, 3, 5, 7, 9, 11, 13])
+        window_size = 2
+
+        expected_X = []
+        expected_X.append(odd_nums[0:2])
+        expected_X.append(odd_nums[1:3])
+        expected_X.append(odd_nums[2:4])
+        expected_X.append(odd_nums[3:5])
+        expected_X.append(odd_nums[4:6])
+
+
+        expected_y = odd_nums[2:]
+
+        expected_X = np.asarray(expected_X)
+        expected_y = np.asarray(expected_y)
+        expected_y = np.reshape(expected_y, (len(expected_y), 1))  # optional
+
+        # assert (type(expected_X).__name__ == 'ndarray')
+        # assert (type(expected_y).__name__ == 'ndarray')
+        # assert (expected_X.shape == (5, 2))
+        # assert (expected_y.shape in [(5, 1), (5,)])
+
+        actual_X, actual_y = window_transform_series(odd_nums, window_size)
+
+        np.testing.assert_array_equal(actual_X, expected_X)
+        np.testing.assert_array_equal(actual_y, expected_y)
+
+    def test_window_transform_text(self):
+        # first 10 alphabet
+        text = "abcdefghij"
+
+        assert len(text) == 10
+
+        window_size = 5
+        step_size = 3
+
+        expected_input = [
+            ['a', 'b', 'c', 'd', 'e'],
+            ['b', 'c', 'd', 'e', 'f'],
+            ['c', 'd', 'e', 'f', 'g']
+        ]
+
+        expected_output = [
+            ['f', 'g', 'h'],
+            ['g', 'h', 'i'],
+            ['h', 'i', 'j']
+        ]
+
+        actual_input, actual_output = window_transform_text(
+            text, window_size, step_size)
+
+        self.assertListEqual(actual_input, expected_input)
+        self.assertListEqual(actual_output, expected_output)
